73a1e9cfdea61b41f3da26b3f34fe62a61f1c460
KrasuckiD <darasmm@gmail.com>
2018-03-15 18:38:56 +0100

    sum class

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -4,4 +4,13 @@ public class Calculator {
     public int sum(int a, int b){
         return a+b;
     }
+    public int div(int a, int b){
+        return a/b;
+    }
+    public int mul(int a, int b){
+        return a*b;
+    }
+    public int min(int a, int b){
+        return a-b;
+    }
 }
--- src/sda/App.java
+++ src/sda/App.java
@@ -1,7 +1,12 @@
 package sda;
 
+import calculator.Calculator;
+
 public class App {
     public static void main(String[] args) {
-
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.sum(5,10));
+        System.out.println(calculator.div(5,5));
+        System.out.println(calculator.mul(5,5));
     }
 }

ce881606d0ebcc9a0456d146f14c741c77b22351
KrasuckiD <36706147+KrasuckiD@users.noreply.github.com>
2018-03-15 19:12:12 +0100

    sum class (#1)

---
+++ src/calculator/RandomGenerator.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import java.util.Random;
+
+public class RandomGenerator {
+    private Random random;
+    public RandomGenerator (){
+        this.random = new Random();
+    }
+
+    public int getSmallNumber (){
+        return random.nextInt(15);
+    }
+    public int getBigNumber(){
+        return random.nextInt(1000);
+    }
+}

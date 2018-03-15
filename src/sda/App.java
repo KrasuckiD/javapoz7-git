65a4f3944abc4e7405f52e4e5c03cfa87f416b4b
KrasuckiD <darasmm@gmail.com>
2018-03-15 20:12:04 +0100

    myCalculator features

--- src/sda/App.java
+++ src/sda/App.java
@@ -5,12 +5,13 @@ import calculator.RandomGenerator;
 
 public class App {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
+        Calculator myCalculator = new Calculator();
         RandomGenerator randomGenerator = new RandomGenerator();
-        System.out.println(calculator.sum(randomGenerator.getSmallNumber(),randomGenerator.getBigNumber()));
-        System.out.println(calculator.div(randomGenerator.getSmallNumber(),randomGenerator.getSmallNumber()));
-        System.out.println(calculator.mul(randomGenerator.getSmallNumber(),randomGenerator.getSmallNumber()));
-        System.out.println("Factorial = " + calculator.factorisl(5));
+        System.out.println(myCalculator.sum(randomGenerator.getSmallNumber(),randomGenerator.getBigNumber()));
+        System.out.println(myCalculator.div(randomGenerator.getSmallNumber(),randomGenerator.getSmallNumber()));
+        System.out.println(myCalculator.mul(randomGenerator.getSmallNumber(),randomGenerator.getSmallNumber()));
+        System.out.println("Factorial = " + myCalculator.factorisl(5));
+        System.out.println("Hello");
 
 
     }
